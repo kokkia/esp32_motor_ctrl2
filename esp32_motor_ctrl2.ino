4b331572f19fd3c153b84b52311ea89b23831bfc
kokkia <opportunity.curiosity@gmail.com>
2019-07-10 01:50:19 +0900

    2motor PID制御実験
    Signed-off-by: kokkia <opportunity.curiosity@gmail.com>

--- esp32_motor_ctrl2.ino
+++ esp32_motor_ctrl2.ino
@@ -3,7 +3,8 @@
 #define DEBUG 1
 
 //motor
-kal::nxtmotor motor[4];//4 motor
+#define MOTOR_NUM 2
+kal::nxtmotor motor[MOTOR_NUM];//4 motor
 //motor control gain
 #define KP 90.0
 #define KD 0.2
@@ -13,12 +14,14 @@ kal::nxtmotor motor[4];//4 motor
 kal::wave sin_wave(0.0,PI/3,0.5,SIN);
 
 //robotdata
-kal::RobotData ref;
-kal::RobotData state;
+kal::RobotData ref[MOTOR_NUM];
+kal::RobotData state[MOTOR_NUM];
 
 //differentiator
-kal::Diff<double> dtheta_st(0.0,100.0);
-kal::Diff<double> dtheta_ref(0.0,100.0);
+//kal::Diff<double> dtheta_st(0.0,100.0);
+//kal::Diff<double> dtheta_ref(0.0,100.0);
+kal::Diff<double> dtheta_st[MOTOR_NUM];
+kal::Diff<double> dtheta_ref[MOTOR_NUM];
 
 double t = 0.0;//time
 
@@ -31,37 +34,64 @@ void IRAM_ATTR onTimer() {  /* this function must be placed in IRAM */
   //control---------------------------------------------------------------------------------------------------------------------------/
   t += Ts;
   //状態取得
-  motor[0].get_angle(PCNT_UNIT_0,state.theta);
-  dtheta_st.update(state.theta,state.dtheta);
+  motor[0].get_angle(PCNT_UNIT_0,state[0].theta);
+  motor[1].get_angle(PCNT_UNIT_1,state[1].theta);
+//  motor[2].get_angle(PCNT_UNIT_2,state[2].theta);
+//  motor[3].get_angle(PCNT_UNIT_3,state[3].theta);
+  for(int i=0;i<MOTOR_NUM;i++){
+    dtheta_st[i].update(state[i].theta,state[i].dtheta);  
+  }  
   
   //目標値計算
   sin_wave.update();
-  ref.theta = sin_wave.output;
-  dtheta_ref.update(ref.theta,ref.dtheta);
+  for(int i=0;i<MOTOR_NUM;i++){
+    ref[i].theta = sin_wave.output;
+    dtheta_ref[i].update(ref[i].theta,ref[i].dtheta);
+  }
   
   //出力計算
-  double u = KP*(ref.theta-state.theta) + KD * (ref.dtheta - state.dtheta);
-  motor[0].drive(u);
-
-#if DEBUG    
-  Serial.print(ref.theta*RAD2DEG);
-  Serial.print(",");
-  Serial.println(state.theta*RAD2DEG);
+  for(int i=0;i<MOTOR_NUM;i++){
+    double u = KP*(ref[i].theta-state[i].theta) + KD * (ref[i].dtheta - state[i].dtheta);
+    motor[i].drive(u);
+  }
+#if DEBUG
+  for(int i=0;i<MOTOR_NUM;i++){
+    Serial.print(ref[i].theta*RAD2DEG);
+    Serial.print(",");
+    Serial.print(state[i].theta*RAD2DEG);     
+    Serial.print(",");
+  }
+  Serial.println();
 #endif
   //-------------------------------------------------------------------------------------------------------------------------------------/
   portEXIT_CRITICAL_ISR(&timerMux);
-  }
+}
 
 
 void setup() {
   Serial.begin(115200);
   Serial.println("started");
   
-  //motorの設定
-  motor[0].GPIO_setup(GPIO_NUM_14,GPIO_NUM_27);//方向制御ピン設定
-  motor[0].PWM_setup(GPIO_NUM_12,0);//PWMピン設定
-  motor[0].encoder_setup(PCNT_UNIT_0,GPIO_NUM_25,GPIO_NUM_26);//エンコーダカウンタ設定
-
+  //motor1の設定
+  motor[0].GPIO_setup(GPIO_NUM_4,GPIO_NUM_0);//方向制御ピン設定
+  motor[0].PWM_setup(GPIO_NUM_2,0);//PWMピン設定
+  motor[0].encoder_setup(PCNT_UNIT_0,GPIO_NUM_36,GPIO_NUM_39);//エンコーダカウンタ設定
+//  motor[0].GPIO_setup(GPIO_NUM_14,GPIO_NUM_27);//方向制御ピン設定
+//  motor[0].PWM_setup(GPIO_NUM_12,0);//PWMピン設定
+//  motor[0].encoder_setup(PCNT_UNIT_0,GPIO_NUM_25,GPIO_NUM_26);//エンコーダカウンタ設定
+  //motor2
+  motor[1].GPIO_setup(GPIO_NUM_16,GPIO_NUM_17);//方向制御ピン設定
+  motor[1].PWM_setup(GPIO_NUM_15,0);//PWMピン設定
+  motor[1].encoder_setup(PCNT_UNIT_1,GPIO_NUM_34,GPIO_NUM_35);//エンコーダカウンタ設定
+/*  //motor3
+  motor[2].GPIO_setup(GPIO_NUM_14,GPIO_NUM_27);//方向制御ピン設定
+  motor[2].PWM_setup(GPIO_NUM_13,0);//PWMピン設定
+  motor[2].encoder_setup(PCNT_UNIT_2,GPIO_NUM_32,GPIO_NUM_33);//エンコーダカウンタ設定
+  //motor4
+  motor[3].GPIO_setup(GPIO_NUM_22,GPIO_NUM_23);//方向制御ピン設定
+  motor[3].PWM_setup(GPIO_NUM_12,0);//PWMピン設定
+  motor[3].encoder_setup(PCNT_UNIT_3,GPIO_NUM_25,GPIO_NUM_26);//エンコーダカウンタ設定
+*/
   //timer割り込み設定
   timer = timerBegin(0, 80, true);//プリスケーラ設定
   timerAttachInterrupt(timer, &onTimer, true);//割り込み関数指定
