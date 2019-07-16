a71051cee12459958bfbd2e4171e943ff64330c9
kokkia <opportunity.curiosity@gmail.com>
2019-07-16 11:32:42 +0900

    4 motors control success

--- esp32_motor_ctrl2.ino
+++ esp32_motor_ctrl2.ino
@@ -1,13 +1,14 @@
 #include "kal/kal.h"
 
 #define DEBUG 1
+#define ADC_DEBUG 0
 
 //motor
-#define MOTOR_NUM 2
+#define MOTOR_NUM 4
 kal::nxtmotor motor[MOTOR_NUM];//4 motor
 //motor control gain
-#define KP 90.0
-#define KD 0.2
+#define KP 30.0
+#define KD 0.0
 #define KDD 0.05
 
 //reference
@@ -36,8 +37,8 @@ void IRAM_ATTR onTimer() {  /* this function must be placed in IRAM */
   //状態取得
   motor[0].get_angle(PCNT_UNIT_0,state[0].theta);
   motor[1].get_angle(PCNT_UNIT_1,state[1].theta);
-//  motor[2].get_angle(PCNT_UNIT_2,state[2].theta);
-//  motor[3].get_angle(PCNT_UNIT_3,state[3].theta);
+  motor[2].get_angle(PCNT_UNIT_2,state[2].theta);
+  motor[3].get_angle(PCNT_UNIT_3,state[3].theta);
   for(int i=0;i<MOTOR_NUM;i++){
     dtheta_st[i].update(state[i].theta,state[i].dtheta);  
   }  
@@ -63,6 +64,16 @@ void IRAM_ATTR onTimer() {  /* this function must be placed in IRAM */
   }
   Serial.println();
 #endif
+#if ADC_DEBUG
+  int ain1 = analogRead(14);
+  int ain2 = analogRead(27);
+  Serial.print(ain1);
+  Serial.print(",");
+  Serial.print(ain2);     
+  Serial.print(",");
+
+  Serial.println();
+#endif
   //-------------------------------------------------------------------------------------------------------------------------------------/
   portEXIT_CRITICAL_ISR(&timerMux);
 }
@@ -83,15 +94,16 @@ void setup() {
   motor[1].GPIO_setup(GPIO_NUM_16,GPIO_NUM_17);//方向制御ピン設定
   motor[1].PWM_setup(GPIO_NUM_15,0);//PWMピン設定
   motor[1].encoder_setup(PCNT_UNIT_1,GPIO_NUM_34,GPIO_NUM_35);//エンコーダカウンタ設定
-/*  //motor3
-  motor[2].GPIO_setup(GPIO_NUM_14,GPIO_NUM_27);//方向制御ピン設定
+  //motor3
+  //motor[2].GPIO_setup(GPIO_NUM_14,GPIO_NUM_27);//方向制御ピン設定
+  motor[2].GPIO_setup(GPIO_NUM_5,GPIO_NUM_21);//方向制御ピン設定
   motor[2].PWM_setup(GPIO_NUM_13,0);//PWMピン設定
   motor[2].encoder_setup(PCNT_UNIT_2,GPIO_NUM_32,GPIO_NUM_33);//エンコーダカウンタ設定
   //motor4
   motor[3].GPIO_setup(GPIO_NUM_22,GPIO_NUM_23);//方向制御ピン設定
   motor[3].PWM_setup(GPIO_NUM_12,0);//PWMピン設定
   motor[3].encoder_setup(PCNT_UNIT_3,GPIO_NUM_25,GPIO_NUM_26);//エンコーダカウンタ設定
-*/
+
   //timer割り込み設定
   timer = timerBegin(0, 80, true);//プリスケーラ設定
   timerAttachInterrupt(timer, &onTimer, true);//割り込み関数指定
